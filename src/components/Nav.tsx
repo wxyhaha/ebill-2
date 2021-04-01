e44f737b86245e9d6e5e3e4930d2f4f921fb599b
joel <1666464510@qq.com>
2021-04-01 22:53:37 +0800

    icons完成

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
+require('icons/money.svg');
+require('icons/label.svg');
+require('icons/statistics.svg');
+
 
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
 
   > ul {
     display: flex;
@@ -12,27 +16,44 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.3%;
       text-align: center;
-      padding: 16px;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      padding: 4px 0;
+      .icon{
+        width: 24px;
+        height: 24px;
+      }
     }
   }
 `;
 
-const Nav=()=>{
-  return(
+const Nav = () => {
+  return (
     <NavWrapper>
       <ul>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#label'/>
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#money'/>
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg className='icon'>
+            <use xlinkHref='#statistics'/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
     </NavWrapper>
-  )
-}
+  );
+};
 
 export default Nav;
