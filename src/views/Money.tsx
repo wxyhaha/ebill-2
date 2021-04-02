74f5d42da43800744e0cb6b2582339128a51c5f1
joel <1666464510@qq.com>
2021-04-02 21:00:48 +0800

    完成Money.tsx的UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,11 @@ import styled from 'styled-components';
 const TagsSection = styled.section`
   background: #ffffff;
   padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
 
   > ol {
     margin: 0 -12px;
@@ -51,7 +56,6 @@ const NotesSection = styled.section`
     }
   }
 `;
-
 const CategorySection = styled.section`
   font-size: 24px;
 
@@ -78,7 +82,6 @@ const CategorySection = styled.section`
     }
   }
 `;
-
 const NumberPadSection = styled.section`
   display: flex;
   flex-direction: column;
@@ -139,10 +142,14 @@ const NumberPadSection = styled.section`
   }
 `;
 
+const MyLayout=styled(Layout)`
+  display: flex;
+  flex-direction: column;
+`
 
 function Money() {
   return (
-    <Layout>
+    <MyLayout>
       <TagsSection>
         <ol>
           <li>衣</li>
@@ -185,7 +192,7 @@ function Money() {
           <button>.</button>
         </div>
       </NumberPadSection>
-    </Layout>
+    </MyLayout>
   );
 }
 
