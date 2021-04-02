9c694012d640c69242c5c5f2afdf082df117fa7e
joel <1666464510@qq.com>
2021-04-02 14:09:58 +0800

    创建views目录

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from 'react-router-dom';
-import Layout from './components/Layout';
+import Tags from './views/Tags';
+import Money from './views/Money';
+import Statistics from './views/Statistics';
+import NoMatch from './views/NoMatch';
 
 
 
@@ -29,34 +31,6 @@ function App() {
         </Route>
       </Switch>
     </Router>
-  );
-}
-
-function NoMatch() {
-  return <div>页面不存在</div>;
-}
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计</h2>
-    </Layout>
-  );
-}
-
-function Tags() {
-  return (
-    <Layout>
-      <h2>标签</h2>
-    </Layout>
-  );
-}
-
-function Money() {
-  return (
-    <Layout>
-      <h2>记账</h2>
-    </Layout>
   );
 }
 
