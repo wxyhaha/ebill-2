816f093879539eed21b6db51b6894cdca8c7475d
joel <1666464510@qq.com>
2021-04-02 16:35:22 +0800

    完成Money.tsx的TagsSection部分UI

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -3,7 +3,29 @@ import React from 'react';
 import styled from 'styled-components';
 
 const TagsSection = styled.section`
+  background: #ffffff;
+  padding: 12px 16px;
 
+  > ol {
+    margin: 0 -12px;
+
+    > li {
+      background: #d9d9d9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+    }
+  }
+  > button{
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 8px;
+  }
 `;
 const NotesSection = styled.section`
 
@@ -16,6 +38,7 @@ const CategorySection = styled.section`
 const NumberPadSection = styled.section`
 
 `;
+
 
 function Money() {
   return (
