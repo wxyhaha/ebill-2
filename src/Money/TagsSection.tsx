286c3d958dc144f4571f9a825d60e86930df2bd8
joel <1666464510@qq.com>
2021-04-02 22:45:15 +0800

    完成标签的选中和新增功能

---
+++ src/Money/TagsSection.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import React, {useState} from 'react';
+
+const Wrapper = styled.section`
+  background: #ffffff;
+  padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
+
+  > ol {
+    margin: 0 -12px;
+
+    > li {
+      background: #d9d9d9;
+      border-radius: 18px;
+      display: inline-block;
+      padding: 3px 18px;
+      font-size: 14px;
+      margin: 8px 12px;
+      &.selected{
+        background: #339cff;
+      }
+    }
+  }
+
+  > button {
+    background: none;
+    border: none;
+    padding: 2px 4px;
+    border-bottom: 1px solid #333;
+    color: #666;
+    margin-top: 8px;
+  }
+`;
+
+const TagsSection: React.FC = (props) => {
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行']);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const onAddTag = () => {
+    const tagName = window.prompt('请输入新标签');
+    if (tagName !== null) {
+      setTags([...tags, tagName]);
+    }
+  };
+  const onToggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag);
+    if (index >= 0) {
+      setSelectedTags(selectedTags.filter(t => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map(tag =>
+          <li key={tag} onClick={() => {onToggleTag(tag);}} className={selectedTags.indexOf(tag)>=0 ? 'selected' : ''}>{tag}</li>
+        )}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  );
+};
+
+
+export default TagsSection;
