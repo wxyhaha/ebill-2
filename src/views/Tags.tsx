41f9fb99688235249c64a9beb6d470a09e2c0d00
joel <1666464510@qq.com>
2021-04-03 23:05:50 +0800

    创建/tags/:tag路由

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import useTags from '../useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import {Link} from 'react-router-dom';
 
 const TagList = styled.ol`
   font-size: 16px;
@@ -11,11 +12,14 @@ const TagList = styled.ol`
   > li {
     border-bottom: 1px solid #d5d5d5;
     line-height: 20px;
-    padding: 12px 16px 12px 0;
     margin-left: 16px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    > a{
+      padding: 12px 16px 12px 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      
+    }
   }
 `;
 
@@ -24,10 +28,9 @@ const Button = styled.button`
   background: #f5f5f5;
   padding: 8px 0;
 
-  svg {
-    height: 48px;
-    width: 48px;
-    border: none;
+  > svg {
+    height: 36px;
+    width: 36px;
     color: #666666;
 `;
 
@@ -37,7 +40,7 @@ const Center = styled.div`
   align-items: center;
   flex-direction: column;
 `;
-const Space=styled.div`
+const Space = styled.div`
   height: 32px;
 `;
 
@@ -48,8 +51,10 @@ function Tags() {
       <TagList>
         {tags.map(tag =>
           <li key={tag}>
-            <span className='oneLine'>{tag}</span>
-            <Icon name='right'/>
+            <Link to={'/tags/' + tag}>
+              <span className='oneLine'>{tag}</span>
+              <Icon name='right'/>
+            </Link>
           </li>
         )}
       </TagList>
