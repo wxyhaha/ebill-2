c71b52e562c5126e2ade5789251abd22acc24dd7
joel <1666464510@qq.com>
2021-04-03 14:37:52 +0800

    numberPad功能基本完成

--- src/Money/NumberPadSection.tsx
+++ src/Money/NumberPadSection.tsx
@@ -71,11 +71,19 @@ const Wrapper = styled.section`
 `;
 
 const NumberPadSection: React.FC = () => {
-  const [output,setOutput]=useState('0')
-  const onClickButtonWrapper=(e: React.MouseEvent)=>{
-    const text=(e.target as HTMLButtonElement).textContent
-    if(text===null){return;}
-    switch (text){
+  const [output, _setOutput] = useState('0');
+  const setOutput=(output:string)=>{
+    if(output.length>16){
+      output=output.slice(0,16)
+    }else if(output.length===0){
+      output='0';
+    }
+    _setOutput(output)
+  }
+  const onClickButtonWrapper = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent;
+    if (text === null) {return;}
+    switch (text) {
       case '0':
       case '1':
       case '2':
@@ -86,25 +94,31 @@ const NumberPadSection: React.FC = () => {
       case '7':
       case '8':
       case '9':
-      case '.':
-        if(output==='0'){
-          setOutput(text)
-        }else{
-          setOutput(output+text)
+        if (output === '0') {
+          setOutput(text);
+        } else {
+          setOutput(output + text);
         }
-        console.log(text);
+        break;
+      case '.':
+        if (output.indexOf('.') >= 0) {return;}
+        setOutput(output + '.');
         break;
       case '删除':
-        console.log('删除');
+        if (output.length === 1) {
+          setOutput('');
+        } else {
+          setOutput(output.slice(0, -1));
+        }
         break;
       case '清空':
-        console.log('清空');
+        setOutput('');
         break;
       case 'OK':
         console.log('确认');
         break;
     }
-  }
+  };
   return (
     <Wrapper>
       <div className='output'>
