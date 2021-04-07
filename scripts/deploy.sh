e829f4695d9685b653c608ee5eed5d3fe92438b3
joel <1666464510@qq.com>
2021-04-07 23:22:57 +0800

    添加deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,9 @@ git init &&
 git add . &&
 git commit -m 'deploy' &&
 git remote add origin git@github.com:wxyhaha/ebill-2-website.git &&
-git push -u origin master -f
+git push -u origin master -f &&
+git remote add gitee git@gitee.com:joelW/ebill-2-website.git &&
+git push -u gitee master -f
 cd -
 
 
