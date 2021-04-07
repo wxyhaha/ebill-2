31ef75debd1acdb8e77c0449e6226b6db5b47ff1
joel <1666464510@qq.com>
2021-04-07 23:28:36 +0800

    添加deploy

--- scripts/deploy-sourceCode.sh
+++ scripts/deploy-sourceCode.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-git remote add origin git@github.com:wxyhaha/ebill-2.git &&
 git push -u origin master &&
-git remote add gitee git@gitee.com:joelW/ebill-2.git &&
 git push -u gitee master
