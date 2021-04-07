d479ad603c2c19381a3f95b60d161078acadcb59
joel <1666464510@qq.com>
2021-04-07 22:00:30 +0800

    优化页面滚动逻辑

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import Nav from './Nav';
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -13,16 +13,31 @@ const Main = styled.div`
   overflow: auto;
 `;
 
+type Props = {
+  className?: string;
+  scrollTop?: number;
+}
 
-const Layout=(props:any)=>{
-  return(
+const Layout: React.FC<Props> = (props) => {
+  const mainRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    setTimeout(()=>{
+      if (!mainRef.current) {return;}
+      mainRef.current.scrollTop = props.scrollTop!;
+    }, 0);
+    },[props.scrollTop])
+  return (
     <Wrapper>
-      <Main className={props.className}>
+      <Main ref={mainRef} className={props.className}>
         {props.children}
       </Main>
       <Nav/>
     </Wrapper>
-  )
+  );
+};
+
+Layout.defaultProps={
+  scrollTop:0
 }
 
 export default Layout;
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -40,7 +40,7 @@ function Money() {
     }
   }
   return (
-    <MyLayout>
+    <MyLayout scrollTop={9999}>
       <TagsSection value={selected.tagIds} onChange={tagIds=>onChange({tagIds})}/>
       <NotesSection value={selected.note} onChange={note=>onChange({note})}/>
       <CategoryWrapper>
